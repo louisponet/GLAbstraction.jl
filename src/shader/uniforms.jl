897c15ec73470ed7cf7ee93bd8df7a477e43173f
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2019-03-26 17:03:55 +0100

    replace gluniform method to use appropriate glUniformxyfv method for Arrays of different sizes

--- src/shader/uniforms.jl
+++ src/shader/uniforms.jl
@@ -26,7 +26,42 @@ gluniform(location::Integer, x::GLfloat)                             = glUniform
 gluniform(location::Integer, x::GLdouble)                            = glUniform1d(GLint(location),  x)
 
 #Uniform upload functions for julia arrays...
-gluniform(location::GLint, x::Vector{Float32})                       = glUniform1fv(location, length(x), x)
+function gluniform(location::GLint, x::AbstractVector{Float32})
+    N = length(x)
+    if N == 2
+        glUniform2fv(location, 1, x)
+    elseif N == 3
+        glUniform3fv(location, 1, x)
+    elseif N == 4
+        glUniform4fv(location, 1, x)
+    else
+        glUniform1fv(location, N, x)
+    end
+end
+function gluniform(location::GLint, x::AbstractMatrix{Float32})
+    N, M = size(x)
+    if N == 2
+        if M == 2
+            glUniformMatrix2fv(location, 1, GL_FALSE, x)
+        else
+            glUniform2fv(location, M, x)
+        end
+    elseif N == 3
+        if M == 3
+            glUniformMatrix3fv(location, 1, GL_FALSE, x)
+        else
+            glUniform3fv(location, M, x)
+        end
+    elseif N == 4
+        if M == 4
+            glUniformMatrix4fv(location, 1, GL_FALSE, x)
+        else
+            glUniform4fv(location, M, x)
+        end
+    else
+        glUniform1fv(location, N*M, x)
+    end
+end
 gluniform(location::GLint, x::Vector{GLdouble})                      = glUniform1dv(location, length(x), x)
 gluniform(location::GLint, x::Vector{GLint})                         = glUniform1iv(location, length(x), x)
 gluniform(location::GLint, x::Vector{GLuint})                        = glUniform1uiv(location, length(x), x)
